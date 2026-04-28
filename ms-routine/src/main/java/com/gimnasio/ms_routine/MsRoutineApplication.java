acaf5cf44e61364bbf2f3a48a41fb7e77ec56922
lich73 <d.aedo.esc3@gmail.com>
2026-04-28 11:50:23 -0400

    Microservicio 4 - Rutina

---
+++ ms-routine/src/main/java/com/gimnasio/ms_routine/MsRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_routine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRoutineApplication.class, args);
+	}
+
+}
---
+++ ms-routine/src/test/java/com/gimnasio/ms_routine/MsRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
