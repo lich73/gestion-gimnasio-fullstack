06c084a2988344a81b17f84c8cb432a3acea81b2
lich73 <d.aedo.esc3@gmail.com>
2026-04-28 11:33:38 -0400

    Microservicio 2 - Usuarios
    Administrador, cliente, empleado.

---
+++ ms-users/src/test/java/com/gimnasio/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
