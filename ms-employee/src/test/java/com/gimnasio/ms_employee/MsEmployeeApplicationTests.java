d7bb4613de4745ae3884bd5d821dba8d68baa495
lich73 <d.aedo.esc3@gmail.com>
2026-04-28 11:45:32 -0400

    Microservicio 1 - Empleados
    Vacío

---
+++ ms-employee/src/test/java/com/gimnasio/ms_employee/MsEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
