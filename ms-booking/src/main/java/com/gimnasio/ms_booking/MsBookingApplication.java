fc833f829f4b3798520513bb4d3ea1d8ae0bbe28
lich73 <d.aedo.esc3@gmail.com>
2026-04-28 11:51:20 -0400

    Microservicio 6 - Reservas

---
+++ ms-booking/src/main/java/com/gimnasio/ms_booking/MsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBookingApplication.class, args);
+	}
+
+}
---
+++ ms-booking/src/test/java/com/gimnasio/ms_booking/MsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
