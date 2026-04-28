90a04e1241bb07b8edc787e8461c95403e760fe0
lich73 <d.aedo.esc3@gmail.com>
2026-04-28 11:52:49 -0400

    Microservicio 8 - Asistencia

---
+++ ms-attendance/src/test/java/com/gimnasio/ms_attendance/MsAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasio.ms_attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
